03ddd78bb34f5c24cfc8a348ef72cc8ed00d74a9
Prasad Niak <prasadniak@gmail.com>
2015-11-25 22:39:03 +0000

    Added the index to show list of all created index , and indeed define function for it in controller , also made some modifications in some of views of articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,10 @@ class ArticlesController < ApplicationController
         end
     end
     
+    def index
+       @article = Article.all 
+    end
+    
     def show
         @article = Article.find(params[:id])
     end

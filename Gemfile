c84272a46bbcc60bc6ac471b3304c11850fa1252
Prasad Niak <prasadniak@gmail.com>
2015-11-27 01:18:51 +0000

    Added the Secure Password

--- Gemfile
+++ Gemfile
@@ -29,6 +29,9 @@ gem 'figaro', '~> 1.0.0'
 # Use Paperclip for picture upload
 gem 'paperclip', '~> 4.2.1'
 
+# Use bcrypt for password encryption
+gem 'bcrypt', '~> 3.1.7'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126232309) do
+ActiveRecord::Schema.define(version: 20151127004335) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20151126232309) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
